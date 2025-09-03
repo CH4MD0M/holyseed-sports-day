4da8868d01c5399074e5365f9632d8e1c630a00f
Kihoon Roh <started677@gmail.com>
2025-09-03 19:12:47 +0900

    feat: auth middleware 구현

---
+++ src/utils/supabase/middleware.ts
@@ -0,0 +1,46 @@
+import { createServerClient } from '@supabase/ssr';
+import { NextResponse, type NextRequest } from 'next/server';
+
+const protectedRoutes = ['/admin', '/'];
+
+export async function updateSession(request: NextRequest) {
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value));
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
+          );
+        },
+      },
+    }
+  );
+  const pathname = request.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(pathname);
+  const isAuthRoute = pathname.startsWith('/login');
+
+  const session = await supabase.auth.getUser();
+
+  if (isProtectedRoute && session.error) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  if (isAuthRoute && !session.error) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  return supabaseResponse;
+}
