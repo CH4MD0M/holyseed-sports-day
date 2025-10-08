40fe0f214a3dc7108b36e2d4147b90fe8976a51a
Kihoon Roh <started677@gmail.com>
2025-10-08 18:23:17 +0900

    refactor: manifest 파일 수정

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,12 +2,10 @@ import type { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Holyseed 청년 2,3부 연합 체육대회',
-    short_name: 'Holyseed 체육대회',
-    start_url: '/',
     display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#2563eb', // 파란색 계열 (체육대회 느낌)
+    lang: 'ko',
+    name: 'Holy seed 체육대회',
+    short_name: 'Holyseed',
     icons: [
       {
         src: '/icon-192x192.png',
