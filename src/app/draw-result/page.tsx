6dd48baee75de42dd77bc29547a8cc28bd8732df
Kihoon Roh <started677@gmail.com>
2025-10-10 00:46:54 +0900

    feat: 추첨 결과 페이지 생성

---
+++ src/app/draw-result/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import ComingSoonPage from '@/components/coming-soon/coming-soon';
+
+const DrawResultPage = () => {
+  return <ComingSoonPage />;
+};
+
+export default DrawResultPage;
